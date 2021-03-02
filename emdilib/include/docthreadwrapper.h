df2edb9b2bf004f19d523fbfc136fe054d7d3ee4
Michael Schwager <mschw@fb.com>
2021-03-01 22:00:03 -0800

    Changes

--- emdilib/include/docthreadwrapper.h
+++ emdilib/include/docthreadwrapper.h
@@ -2,7 +2,9 @@
 #define DOCTHREADWRAPPER_H
 
 #include "idocument.h"
+#include <QDebug>
 #include <QObject>
+#include <QThread>
 #include <memory>
 
 // Intended to wrap an instance of IDocument
@@ -20,26 +22,44 @@ class DocThreadWrapper : public QObject
     Q_OBJECT
 private:
     std::unique_ptr<IDocument> m_doc;
+    QThread *m_thread;
+
 public:
-    DocThreadWrapper(const DocThreadWrapper & ) {}
     DocThreadWrapper(std::unique_ptr<IDocument> dp) :
-        m_doc(std::move(dp))
+        m_doc(std::move(dp)),
+        m_thread(new QThread)
     {
         m_doc->setWrapper(this);
+        moveToThread(m_thread);
+        connect(m_thread, &QThread::finished, []{
+            qDebug() << "Threadwrapper thread finished";
+        });
+        m_thread->start();
+        qDebug() << "Wrapper thread" << m_thread;
     }
-    IDocument *get() const {return m_doc.get();}
+    DocThreadWrapper(const DocThreadWrapper & ) {}
+    ~DocThreadWrapper() override {
+        m_thread->quit();
+        m_thread->wait();
+        delete m_thread;
+    }
+    IDocument *doc() const {return m_doc.get();}
 // The slots largely match the IDocument functions
 public slots:
     void init() {m_doc->init();}
     void done() {m_doc->done();}
-    bool isActive() {return m_doc->isActive();}
-    bool supportsUserType(const std::string & userType) {
+    bool isActive(Qt::ConnectionType ct) {return m_doc->isActive();}
+    bool supportsUserType(const std::string & userType, Qt::ConnectionType ct) {
         return m_doc->supportsUserType(userType);}
-    void accept(IDocVisitor *dv) {m_doc->accept(dv);}
-    void accept(const IDocVisitor *dv) {m_doc->accept(dv);}
-    void accept(IDocVisitor *dv) const {m_doc->accept(dv);}
-    void accept(const IDocVisitor *dv) const {m_doc->accept(dv);}
-    void setName(const std::string & name) {m_doc->setName(name);}
+    void accept(IDocVisitor *dv, Qt::ConnectionType ct) {m_doc->accept(dv);}
+    void accept(const IDocVisitor *dv, Qt::ConnectionType ct) {m_doc->accept(dv);}
+    void accept(IDocVisitor *dv, Qt::ConnectionType ct) const {m_doc->accept(dv);}
+    void accept(const IDocVisitor *dv, Qt::ConnectionType ct) const {m_doc->accept(dv);}
+    void setName(const std::string & name, Qt::ConnectionType ct) {m_doc->setName(name);}
+signals:
+    void initDone();
+    void doneDone();
 };
+
 
 #endif // DOCTHREADWRAPPER_H
